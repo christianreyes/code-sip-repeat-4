54f8c9215477567fd25e600d907c7dc34c967695
Christian Reyes <christian.reyes.91@gmail.com>
2024-03-19 14:01:41 -0500

    Added uppercase letter generation and unit test

--- csr_password_gen.py
+++ csr_password_gen.py
@@ -1,9 +1,14 @@
 import random
+import string      # so we can use the pre-defined ascii character lists
 
-lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+lower_letters = string.ascii_lowercase # check the documentation, pre-defined
+upper_letters = string.ascii_uppercase
 
 def random_lower():
     return random.choice(lower_letters)
+
+def random_upper():
+    return random.choice(upper_letters)
 
 def generate_complex_password(complexity_string: str):
     """
@@ -12,8 +17,9 @@ def generate_complex_password(complexity_string: str):
     Inputs:
 
     complexity_string: str
-        Concatination of characters which define the complexity: 
+        Concatination of characters which define the complexity:
         - "L" for a random lowercase letter
+        - "U" for a random uppercase letter
 
     Output:
     A random complex password conforming with the input complexity string requirements
@@ -23,14 +29,15 @@ def generate_complex_password(complexity_string: str):
     for c in complexity_string: # loop through each character of the complexity string
         if c == "L": # if the current character matches "L"
             password_chars.append(random_lower()) # add the random lower letter to the end of the list
+        elif c == "U": # if the current character matches "U"
+            password_chars.append(random_upper()) # add the random upper letter to the end of the list
     
     random.shuffle(password_chars) # randomly shuffle all the characters in the list
     return "".join(password_chars) # combine all the characters in the list into a single string and return it
 
 def main():
-    print("Hello, World! This is the Code.Sip.Repeat Random Password generator")
-    print(f"Your random password is { generate_complex_password("LLLLL") }") # replace with our new function and an input complexity string. "LLLLL" as the example should generate 5 random lowercase letters, shuffled
+    print("Hello, World! This is the Code.Sip.Repeat Random Password Generator")
+    print(f"Your random password is: { generate_complex_password('LLLUUU') }") # replace with our new function and an input complexity string. "LLLUUU" as the example should generate 3 random lowercase and 3 random uppercase letters as the password, shuffled
 
 if __name__ == "__main__":
     main()
-    
--- random_test.py
+++ random_test.py
@@ -1,13 +1,17 @@
-import unittest
-import csr_password_gen
+import unittest           # required to access the test infrastructure functions
+import csr_password_gen   # import the code we previously wrote (random.py must be in the same folder!)
 
 lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+upper_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 class Testing(unittest.TestCase):
    def test_random_lower(self):
        rl = csr_password_gen.random_lower()  # generate random lower letter
        self.assertIn(rl, lower_letters)      # if letter is in the list, return true
 
+   def test_random_upper(self):
+       ru = csr_password_gen.random_upper()  # generate random lower letter
+       self.assertIn(ru, upper_letters)      # if letter is in the list, return true
+
 if __name__ == '__main__':
    unittest.main()                           # run the unit tests if file ran as script
-   
