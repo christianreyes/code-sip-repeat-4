a2918bebb0537374336fd5c5f14a3726e6e5b22e
Christian Reyes <christian.reyes.91@gmail.com>
2024-03-19 01:16:21 -0500

    Added main function

--- csr_password_gen.py
+++ csr_password_gen.py
@@ -4,3 +4,10 @@ lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 
 def random_lower():
     return random.choice(lower_letters)
+
+def main():
+    print("Hello, World! This is the Code.Sip.Repeat Random Password generator")
+    print(f"Your random password is { random_lower() }") # this called a f-string and will run the code in between the {} which will substitute the output of our function
+
+if __name__ == "__main__":
+    main()
