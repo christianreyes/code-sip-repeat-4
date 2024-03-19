5d5ff22877f59a2d593ee9f6c4b1a5b0a60f2ab3
Christian Reyes <christian.reyes.91@gmail.com>
2024-03-19 00:42:45 -0500

    Initial commit of the CSR 4.0 (Code.Secure.Protect) instructional material.

---
+++ csr_password_gen.py
@@ -0,0 +1,6 @@
+import random
+
+lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def random_lower():
+    return random.choice(lower_letters)
